8f883c1ab9dcf8db60f010a6529c85c823772f84
Eric Holtz <holtzeric2@gmail.com>
2025-10-21 12:55:06 -0700

    fix ben death bug

--- scripts/end_trigger.gd
+++ scripts/end_trigger.gd
@@ -6,6 +6,7 @@ extends Area3D
 func _on_body_entered(body: Node3D) -> void:
 	if body.name != "Player":
 		return
+	ben_bot.movement_speed = 0
 	main.player.disable()
 	main.show_end_ui()
 	main.toggle_chase_music(false)
